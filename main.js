4207f6e844c7bf63faecbc858d2f6d4052d2452a
Johan Berndtsson <johanbrndtssn@gmail.com>
2018-03-26 15:16:24 +0200

    fdsf

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
-const LISTEN_PORT = 4567;
+const LISTEN_PORT = process.env.HOST || 4567;
 // const appId = 'dd5b310b-2655-4037-8f16-847f16a585eb';
 
-app.get('/', (req, res) => res.send("Is this working?"));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => res.sendFile('index.html'));
 app.post('/helloworld', (req, res) => {
 
     res.send('Testing, Testing');
