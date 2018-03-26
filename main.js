c308caeeda74bfef0a839211ff11d41b0df74a94
Johan Berndtsson <johanbrndtssn@gmail.com>
2018-03-26 15:34:38 +0200

    fdsfs

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const LISTEN_PORT = process.env.HOST || 8080;
+const LISTEN_PORT = process.env.PORT || 8080;
 // const appId = 'dd5b310b-2655-4037-8f16-847f16a585eb';
 
 app.get('/', (req, res) => res.send('Magnus är så go och gla, när han äter chokela'));
