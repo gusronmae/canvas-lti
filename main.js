740f0e191cf4b99a69e6f13d6f5bdcc48e3ed460
Johan Berndtsson <johanbrndtssn@gmail.com>
2018-03-26 16:19:49 +0200

    got the basics working

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 const LISTEN_PORT = process.env.PORT || 8080;
-// const appId = 'dd5b310b-2655-4037-8f16-847f16a585eb';
 
+app.use(bodyParser.urlencoded({extended: false}));
+
+// The path for 'Account Navigation'
 app.get('/', (req, res) => res.send('Magnus är så go och gla, när han äter chokela'));
 app.post('/helloworld', (req, res) => {
 
     res.send('Testing, Testing');
 });
-app.post('/launch', (req, res) => res.send('Testing again'));
+
+// The path for when a user clicks course menu item on the left
+app.post('/launch', (req, res) => {
+    console.log(JSON.stringify(req.body));
+
+    res.send('Hejsan svejsan');
+});
 
 app.listen(LISTEN_PORT, () => console.log('Listening on port ' + LISTEN_PORT));
